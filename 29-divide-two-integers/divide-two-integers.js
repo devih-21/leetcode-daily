1332307d6406434e4bd502fded464a355c9bf97c
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-02-16 01:13:59 +0700

    Time: 62 ms (80.21%) | Memory: 53 MB (12.57%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.js
+++ 29-divide-two-integers/divide-two-integers.js
@@ -1,6 +1,25 @@
-var divide = function (dividend, divisor) {
-    let result = parseInt(dividend / divisor);
-    if (result > 0x7FFFFFFF) return 0x7FFFFFFF;
-    if (result < -0x7FFFFFFF) return -0x7FFFFFFF - 1;
-    return result;
+const divide = function(dividend, divisor) {
+    const absDivisor = Math.abs(divisor);
+    const absDividend = Math.abs(dividend);
+    let count = 0;
+    let sum = 0
+
+    let countFactor;
+    let sumFactor;
+    while (countFactor !== 1) {
+        countFactor = 1;
+        sumFactor = absDivisor;
+        while (sum + sumFactor <= absDividend) {
+            count += countFactor;
+            sum += sumFactor;
+            countFactor += countFactor;
+            sumFactor += sumFactor;
+        }
+    }
+
+    if (Math.sign(divisor) + Math.sign(dividend) === 0) {
+        return Math.max(-count, -2147483648);
+    } else {
+        return Math.min(count, 2147483647);
+    }
 };
