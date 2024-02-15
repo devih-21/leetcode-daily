e31574f30d6802eab306944013dc118b6ffd9709
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-02-15 17:39:29 +0700

    Time: 62 ms (87.88%) | Memory: 52.1 MB (20.16%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.js
+++ 3226-minimum-number-game/minimum-number-game.js
@@ -3,10 +3,14 @@
  * @return {number[]}
  */
 var numberGame = function(nums) {
-    nums.sort((a, b) => a - b)
-    let result = []
-    for (let i = 0; i < nums.length; i += 2) {
-        result.push(nums[i+1], nums[i])
+    let res = []
+    let alice, bob
+    nums.sort( (a, b) => a - b)
+    for(let i = 0; i < nums.length; i += 2){
+        alice = nums[i]
+        bob = nums[i + 1]
+        res.push(bob)
+        res.push(alice)
     }
-    return result
+    return res
 };
