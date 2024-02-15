ea92e22ce76d7714588b973e9e8dabf73dec582d
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-02-15 17:39:19 +0700

    Time: 67 ms (72.40%) | Memory: 52.4 MB (13.19%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.js
+++ 3226-minimum-number-game/minimum-number-game.js
@@ -3,10 +3,10 @@
  * @return {number[]}
  */
 var numberGame = function(nums) {
-    const tempArr = nums.sort((a, b) => a - b)
+    nums.sort((a, b) => a - b)
     let result = []
     for (let i = 0; i < nums.length; i += 2) {
-        result.push(tempArr[i+1], tempArr[i])
+        result.push(nums[i+1], nums[i])
     }
     return result
 };
