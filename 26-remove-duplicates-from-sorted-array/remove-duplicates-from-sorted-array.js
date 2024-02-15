ebce5967b2ccdb445b0a5d9497493de1b9063935
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-02-15 21:19:21 +0700

    Time: 70 ms (40.86%) | Memory: 52.7 MB (9.90%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -6,8 +6,7 @@ var removeDuplicates = function(nums) {
     let p = 1;
     for (let i = 0; i < nums.length - 1; i++){
         if (nums[i] !== nums[i+1]) {
-                         nums[p]=nums[i+1];
-
+            nums[p]=nums[i+1];
             p++
         }
     }
