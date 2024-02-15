9f71f6ee18cf595fa0de671c55f5dfbfe2fd34a9
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-02-16 01:13:13 +0700

    Time: 75 ms (37.02%) | Memory: 53.1 MB (7.22%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.js
+++ 29-divide-two-integers/divide-two-integers.js
@@ -1,33 +1,6 @@
-function divide(dividend, divisor) {
-    if (dividend === -2147483648 && divisor === -1) {
-        return 2147483647;
-    }
-    
-    let negative = 2;
-    if (dividend > 0) {
-        negative--;
-        dividend = -dividend;
-    }
-    if (divisor > 0) {
-        negative--;
-        divisor = -divisor;
-    }
-
-    let result = dividePositive(dividend, divisor);
-    return negative === 1 ? -result : result;
-};
-
-function dividePositive(dividend, divisor) {
-    if (dividend > divisor) {
-        return 0;
-    }
-
-    let count = 1;
-    let tb = divisor;
-    while (tb >= dividend - tb) {
-        tb += tb;
-        count += count;
-    }
-
-    return count + dividePositive(dividend - tb, divisor);
+var divide = function (dividend, divisor) {
+    let result = parseInt(dividend / divisor);
+    if (result > 0x7FFFFFFF) return 0x7FFFFFFF;
+    if (result < -0x7FFFFFFF) return -0x7FFFFFFF - 1;
+    return result;
 };
