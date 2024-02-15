fe54857528950b061f34c894d3bded5c6349f3ff
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-02-16 01:19:00 +0700

    Time: 33 ms (99.77%) | Memory: 49 MB (21.55%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+ function mergeSort(arr,target,left,right){
+        if(left>right){
+             return left
+        }
+        const mid=Math.floor((left+right)/2)
+        if(arr[mid]==target) return mid
+        else if(arr[mid]<target){
+            return mergeSort(arr,target,mid+1,right)
+        }else{
+            return mergeSort(arr,target,left,mid-1)
+        }
+    }
+
+    return mergeSort(nums,target,0,nums.length-1)
+};
