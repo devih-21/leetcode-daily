a5ea6544317f0d0f3721d9514dd56b465124086d
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-02-17 19:37:36 +0700

    Time: 112 ms (70.44%) | Memory: 55.6 MB (5.02%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.js
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.js
@@ -3,18 +3,33 @@
  * @return {number[][]}
  */
 var findMatrix = function(nums) {
-    let flag = new Array(nums.length + 1).fill(0)
-    let result = []
-    nums.forEach(item => flag[item]++)
-    while (flag.some(item => item !== 0)) {
-        let newItem = flag.map((num, i) => {
-            if (num > 0) {
-                flag[i]--
-                return i
-            }
-        }).filter(item => !!item)
-        console.log(flag)
-        result.push(newItem)
+  // Create an array for the output
+  let output = new Array();
+  // Create a row array
+  let row = new Array();
+  // Add a new row to the output
+  output.push(row);
+  // Create an array for the last used indexes in the output
+  let last = new Array(201).fill(0);
+  // Loop through the input nums
+  for (let i = 0; i < nums.length; i++) {
+    // If the last number is higher than the number of rows
+    if (last[nums[i]] > output.length - 1) {
+      // Create a row array
+      let row = new Array();
+      // Add a new row to the output
+      output.push(row);
     }
-    return result
+    // Otherwise the number needed to be added to the output
+    // Set the index for the insertion
+    let index = last[nums[i]];
+    // Get the the current row using the index number
+    let current = output[index];
+    // Add the number to the corresponding row
+    output[index][current.length] = nums[i];
+    // Increment the index on the last array
+    last[nums[i]]++;
+  }
+  // Return the output
+  return output;
 };
