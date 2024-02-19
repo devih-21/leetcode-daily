ebdadc1cb5982dcb1c7d0ad6b46d5afaedff881a
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-02-19 09:36:50 +0700

    Time: 63 ms (77.05%) | Memory: 53.5 MB (11.00%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -1,13 +1,8 @@
-function getBaseLog(x, y) {
-  return Math.log(y) / Math.log(x);
-}
 
 /**
  * @param {number} n
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    let a = Math.log2(n);
-    console.log("a", a)
-    return Number.isInteger(a)
+    return n >0 ? Number.isInteger(Math.log2(n)) : false
 };
