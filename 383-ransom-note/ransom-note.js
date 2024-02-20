9ccc9f5136a141da9a9fead11c610dc907ccf8c2
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-02-21 01:51:14 +0700

    Time: 89 ms (23.47%) | Memory: 53.1 MB (21.36%) - LeetSync

--- 383-ransom-note/ransom-note.js
+++ 383-ransom-note/ransom-note.js
@@ -5,19 +5,20 @@
  */
 var canConstruct = function(ransomNote, magazine) {
     if (ransomNote.length === 1 && magazine.length === 1 && ransomNote !== magazine) return false
-
-    let mappingRansom = magazine.split("").reduce((pre, cur) => ({...pre, [cur]: pre?.[cur] ? pre[cur] += 1 : 1}), {});
-    console.log("mappingRansom", mappingRansom)
-
-   for (let i = 0; i < ransomNote.length; i++) {
-        if (mappingRansom?.[ransomNote[i]]) {
-            mappingRansom[ransomNote[i]]--;
-        } else {
-            mappingRansom[ransomNote[i]] = -1;
-        }
+    let mappingRansom = {};
+    for (let i = 0; i < magazine.length; i++) {
+        const char = magazine[i];
+        mappingRansom[char] ? mappingRansom[magazine[i]]++ : mappingRansom[magazine[i]] = 1;
     }
-    console.log("mappingRansom", mappingRansom)
-    let arr = Object.values(mappingRansom);
-    if (arr.some(item => item < 0)) return false
-    return true
+
+    for (let i = 0; i < ransomNote.length; i++) {
+            if (mappingRansom?.[ransomNote[i]]) {
+                mappingRansom[ransomNote[i]]--;
+            } else {
+                mappingRansom[ransomNote[i]] = -1;
+            }
+        }
+        let arr = Object.values(mappingRansom);
+        if (arr.some(item => item < 0)) return false
+        return true
 };
