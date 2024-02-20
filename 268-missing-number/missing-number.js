35a4f690969c73ead17b9ec13febfd9f2603d396
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-02-21 01:27:09 +0700

    Time: 62 ms (53.82%) | Memory: 50.6 MB (47.92%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let sum = nums.length * (nums.length+1) / 2;
-    let curSum = nums.reduce((a, b) => a+b, 0)
-    return sum - curSum
+    // let sum = nums.length * (nums.length+1) / 2;
+    // let curSum = nums.reduce((a, b) => a+b, 0)
+    return nums.length * (nums.length+1) / 2 - nums.reduce((a, b) => a+b, 0)
 };
