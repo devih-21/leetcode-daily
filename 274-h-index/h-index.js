50c8e3c27dfe34bed83d1730974adae1f11a369b
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-02-21 17:55:57 +0700

    Time: 54 ms (62.77%) | Memory: 49.3 MB (15.80%) - LeetSync

--- 274-h-index/h-index.js
+++ 274-h-index/h-index.js
@@ -7,7 +7,7 @@ var hIndex = function(citations) {
     let result = 0;
     let n = citations.length;
     for(let i = 0; i < n; i++){
-        result = Math.max(result, Math.min(citations[i], n-i))
+    if (citations[i] >= n - i) result = result < n - i ? n - i : result;
     }
     return result;
     };
