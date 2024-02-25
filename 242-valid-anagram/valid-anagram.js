d048753705f222f2fba45433d2f88455fc624f11
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-02-25 23:53:28 +0700

    Time: 87 ms (25.38%) | Memory: 53.9 MB (20.48%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,5 +4,7 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
+        if (s.length !== t.length) return false;
+
     return t.split("").sort().join("") === s.split("").sort().join("")
 };
