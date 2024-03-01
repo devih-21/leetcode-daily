c08d23fd4f1f6fff41061f8cfc5583da8b7626e6
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-03-01 13:45:57 +0700

    Time: 48 ms (87.80%) | Memory: 50.7 MB (14.82%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -4,9 +4,16 @@
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
+    var jewelSet = new Set()
+    for (let j of jewels) {
+        jewelSet.add(j)
+    }
+
     let count = 0
-    for (let i = 0; i < stones.length; i++) {
-        if (jewels.includes(stones[i])) count++
+    for (let s of stones) {
+        if (jewelSet.has(s)) {
+            count++
+        }
     }
     return count
 };
