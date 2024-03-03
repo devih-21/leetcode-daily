b75c89bbe5ce4f5173228ded9bd310f7fc0c44c5
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-03-04 01:18:27 +0700

    Time: 52 ms (88.08%) | Memory: 49.5 MB (70.04%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.js
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.js
@@ -5,9 +5,6 @@
  */
 var differenceOfSums = function(n, m) {
     let num2 = ((n - n % m) + m) * ((n - n % m)/m) / 2
-    
-   
-    let num1 = (1 + n)*n / 2 - num2
-    console.log(num2, num1)
-return num1 - num2
+    let num1 = (1 + n)*n / 2 
+    return num1 - 2 * num2
 };
