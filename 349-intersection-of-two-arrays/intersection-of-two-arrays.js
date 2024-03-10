73e3134cf73bb59b13842689c63bb3f91030a4cf
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-03-10 20:56:50 +0700

    Time: 52 ms (75.37%) | Memory: 49 MB (73.41%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -6,7 +6,7 @@
 var intersection = function(nums1, nums2) {
     let result = []
     for (let i = 0; i < nums1.length; i++) {
-        if (nums2.includes(nums1[i])) result.push(nums1[i])
+        if (nums2.includes(nums1[i]) && !result.includes(nums1[i])) result.push(nums1[i])
     }
-    return [...new Set(result)]
+    return result
 };
