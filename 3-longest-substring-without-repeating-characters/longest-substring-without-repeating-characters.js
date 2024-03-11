190bbe3c9c38f6b65f04d4b1ba8b1f7fd9a3ba2f
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-03-11 15:11:02 +0700

    Time: 87 ms (52.23%) | Memory: 55.1 MB (37.11%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,21 +3,26 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let maxStr = ""
-    let maxChar = [...new Set([...s])].length;
-    console.log("maxChar", maxChar)
-    for (let i = 0; i < s.length; i++) {
-        let max = s.length - i > maxChar ? maxChar : s.length - i
-        for (let j = 1; j <= max; j++) {
-            let str = s.slice(i, i+j);
-            if (maxStr.length > j) {
-                continue;
-            } else {
-                if (j === [...new Set([...str])].length) {
-                    maxStr = str
-                }
+    
+    const arr = s.split("");
+    let longest = 0;
+    let curr = "";
+
+    arr.forEach(letter => {
+
+        const indexOfLetter = curr.indexOf(letter);
+
+        if(indexOfLetter !== -1) {
+
+            if(curr.length > longest) {
+                longest = curr.length;
             }
+            curr = curr.substring(indexOfLetter + 1, curr.length) + letter;
+            return true;
         }
-    }
-    return maxStr.length
+        
+        curr = curr + letter;
+    });
+
+    return curr.length > longest ? curr.length : longest;
 };
