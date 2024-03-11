95142ee06e5361bcffd014b83529ec95c25de8d7
Khuất Quang Vinh <46088897+devih-21@users.noreply.github.com>
2024-03-11 15:06:50 +0700

    Time: 2037 ms (5.02%) | Memory: 57.6 MB (14.00%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let maxStr = ""
+    for (let i = 0; i < s.length; i++) {
+        let max = s.length - i > 100 ? 100 : s.length - i
+        for (let j = 1; j <= max; j++) {
+            let str = s.slice(i, i+j);
+            if (maxStr.length > j) {
+                continue;
+            } else {
+                if (j === [...new Set([...str])].length) {
+            console.log("str", maxStr)
+
+                    maxStr = str
+                }
+            }
+        }
+    }
+    return maxStr.length
+};
